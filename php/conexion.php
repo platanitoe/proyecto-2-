8db9e9e2ded35ac252fd2bd227930f359bf7882e
platanitoe <eme7866@gmail.com>
2026-07-20 12:39:37 -0600

    actualizacion del index

--- php/conexion.php
+++ php/conexion.php
@@ -1,16 +1,14 @@
 <?php
 
-$servidor="localhost";
-$usuario="root";
-$password="";
-$bd="proyecto";
+$servidor = "localhost";
+$usuario = "root";
+$password = "";
+$bd = "proyecto";
 
-$conexion=new mysqli($servidor,$usuario,$password,$bd);
+$conexion = new mysqli($servidor,$usuario,$password,$bd);
 
 if($conexion->connect_error){
-
-die("Error de conexión");
-
+    die("Error de conexión: ".$conexion->connect_error);
 }
 
 ?>
